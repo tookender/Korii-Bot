889674e674634cb3355e8412dcac0c81eeb104a8
Ender2K89 <ender.yeeterson@gmail.com>
2023-10-06 03:19:39 +0200

    Some new features I guess

--- extensions/utility/__init__.py
+++ extensions/utility/__init__.py
@@ -4,9 +4,10 @@ from .neofetch import NeofetchCog
 from .ping import PingCog
 from .source import SourceCog
 from .embed import EmbedCog
+from .download import DownloadCog
 
 
-class Utility(ConfigCog, InfoCog, NeofetchCog, PingCog, SourceCog, EmbedCog):
+class Utility(ConfigCog, InfoCog, NeofetchCog, PingCog, SourceCog, EmbedCog, DownloadCog):
     pass
 
 
