320947c4bccccbb48e28f2e722c5145e6d3b22f9
tookender <tookender@gmail.com>
2024-05-04 13:58:19 +0200

    👻 Try IPC

---
+++ extensions/events/ipc.py
@@ -0,0 +1,24 @@
+from bot import Korii
+from utils import Cog
+from aiohttp import web
+from aiohttp.web import Response, json_response
+
+
+class IPC(Cog):
+    def __init__(self, bot: Korii):
+        app = web.Application()
+        self.app = app
+        app.add_routes(routes)
+        self.bot.loop.create_task(self._start())
+
+    async def _start(self):
+        await self.bot.wait_until_ready()
+        await web._run_app(self.app, print=None)
+
+
+routes = web.RouteTableDef()
+
+
+@routes.get("/")
+async def ping(_) -> Response:
+    return json_response({"message": f"PONG! {IPC.bot.latency * 1000}"})
