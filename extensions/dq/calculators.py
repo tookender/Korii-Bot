ee5e9054cb4c80a7a2c7d868c31e4042b23fee10
tookender <ender.yeeterson@gmail.com>
2024-09-01 20:09:00 +0200

    💡 Convert string to int

--- extensions/dq/calculators.py
+++ extensions/dq/calculators.py
@@ -6,13 +6,13 @@ from ._base import DQBase
 from utils.calculators import *
 from utils import Embed
 
-
+s!calc_pot 500 1 500
 class CalculatorsCog(DQBase):
 	@commands.hybrid_command(description="Calculate max potential of an item and the upgrade cost.", aliases=["potential", "pot", "calc_pot", "calcpot", "potcalc"])
 	@app_commands.describe(current_power="Current power of your item.")
 	@app_commands.describe(current_upgrades="Current amount of upgrades on your item.")
 	@app_commands.describe(total_upgrades="Total amount of upgrades of your item.")
-	async def calc_potential(self, ctx, current_power, current_upgrades, total_upgrades):
+	async def calc_potential(self, ctx, current_power: int, current_upgrades: int, total_upgrades: int):
 		upgrade_cost = calculate_upgrade_cost(current_upgrades, total_upgrades)
 		potential = calculate_potential(current_power, current_upgrades, total_upgrades)
         
